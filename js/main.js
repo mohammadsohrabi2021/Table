2a2e883a672dc35f7bbbc56b01e0aa700d6342a4
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2022-09-14 22:50:11 +0430

    create button update and create function deletUser and updateUser

--- js/main.js
+++ js/main.js
@@ -27,12 +27,24 @@ function render(data = []) {
      deleteButton.addEventListener("click", function (e) {
        tbody.removeChild(e.target.parentElement.parentElement);
      });
-
+     const updateButton = document.createElement("button");
+     updateButton.classList.add("btn", "btn-success");
+     updateButton.innerText = "UPDATE";
+     updateButton.addEventListener("click", function (e) {
+       submit.value = "update";
+       const row = e.target.parentElement.parentElement;
+       for (let i = 0; i < row.childNodes.length - 1; i++) {
+         const input = document.getElementById(keys[i]);
+         input.value = row.childNodes[i].innerText;
+       }
+     });
+     td.appendChild(updateButton);
     td.appendChild(deleteButton);
     row.appendChild(td);
     tbody.appendChild(row);
   });
 }
+
 
 userForm.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -48,5 +60,12 @@ userForm.addEventListener("submit", function (e) {
   e.target.reset();
 });
 
+function deleteUser(id) {
+  setUsers(users.filter((user) => user.id !== id));
+}
+
+function updateUser(data) {
+  setUsers(users.map((user) => (user.id == data.id ? data : user)));
+}
 
 
