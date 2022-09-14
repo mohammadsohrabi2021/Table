2a42d03b46479831de61f946b93318644d13454f
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2022-09-14 20:49:19 +0430

    add data form to table

---
+++ js/main.js
@@ -0,0 +1,49 @@
+const userForm = document.querySelector("#userForm");
+const tbody = document.querySelector(".table-group-divider");
+const submit = document.querySelector("#userFormSubmit");
+const keys = ["id", "FirstName", "LastName", "Age", "Phone", "Email"];
+
+
+let users = [];
+function setUsers(newUsers) {
+  users = [...newUsers];
+  render(users);
+}
+
+function render(data = []) {
+  tbody.innerHTML = "";
+  data.forEach((user) => {
+    const row = document.createElement("tr");
+    row.id = user.id;
+    for (let key of keys) {
+      const td = document.createElement("td");
+      td.innerText = user[key];
+      row.appendChild(td);
+    }
+    const td = document.createElement("td");
+ 
+    
+
+    row.appendChild(td);
+    tbody.appendChild(row);
+  });
+}
+
+
+
+userForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const data = Object.fromEntries(formData.entries());
+  if (submit.value === "update") {
+    updateUser(data);
+  } else {
+    data.id = Math.floor(Math.random() * 1000);
+    setUsers([...users, data]);
+  }
+  submit.value = "create";
+  e.target.reset();
+});
+
+
+
